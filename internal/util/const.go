9d8ec21c7c4d66893e482f5f0a0838bc7e1ceae9
patch-ridermg48 <cowshed.umlaut35@icloud.com>
2024-11-01 19:56:08 +0100

    ref(const.go): add Invalid UserId error messages

--- internal/util/const.go
+++ internal/util/const.go
@@ -7,6 +7,9 @@ const (
 	DefaultFailedSqlQueryMsg                     string = "An error occurred while executing a SQL query."
 	DefaultFailedScanningToPostgresqlDataTypeMsg string = "An error occurred while scanning the value into a PostgreSQL data type."
 	DefaultFailedFetchingXMRDaemonMsg            string = "An error occurred while fetching."
+
+	InvalidUserIdInvalidUUIDMsg string = "Invalid userId (invalid UUID)."
+	InvalidUserIdUserExistsMsg  string = "Invalid userId (user exists)."
 )
 
 var (
