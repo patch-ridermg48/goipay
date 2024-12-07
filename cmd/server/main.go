473464082603a840e4a469908a5547ecfeb7acc7
patch-ridermg48 <cowshed.umlaut35@icloud.com>
2024-12-07 16:29:41 +0100

    feat(main.go): add clientCAs option for mTLS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,9 +12,13 @@ import (
 
 func main() {
 	configPath := flag.String("config", "config.yml", "Path to the config file")
+	clientCAs := flag.String("client-ca", "", "Comma-separated list of paths to client certificate authority files (for mTLS)")
 	flag.Parse()
 
-	app := app.NewApp(*configPath)
+	app := app.NewApp(app.CliOpts{
+		ConfigPath:    *configPath,
+		ClientCAPaths: *clientCAs,
+	})
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
