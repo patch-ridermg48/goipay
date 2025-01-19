40d82de3d84a9685f8c841bd0690d69911591f30
patch-ridermg48 <cowshed.umlaut35@icloud.com>
2025-01-19 17:23:33 +0100

    feat(processor): add verifyTxHandler and generateNextAddressHandler implementations for LTC

---
+++ internal/processor/ltc.go
@@ -0,0 +1,158 @@
+package processor
+
+import (
+	"context"
+	"errors"
+	"net/url"
+
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/chekist32/goipay/internal/db"
+	"github.com/chekist32/goipay/internal/dto"
+	"github.com/chekist32/goipay/internal/listener"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ltcsuite/ltcd/chaincfg"
+	"github.com/ltcsuite/ltcd/ltcutil"
+	"github.com/ltcsuite/ltcd/ltcutil/hdkeychain"
+	ltcrpc "github.com/ltcsuite/ltcd/rpcclient"
+	"github.com/rs/zerolog"
+)
+
+type ltcProcessor struct {
+	baseCryptoProcessor[listener.LTCTx, listener.LTCBlock]
+}
+
+func verifyLTCTxHandler(ctx context.Context, q *db.Queries, data *verifyTxHandlerData[listener.LTCTx]) (float64, error) {
+	var amount float64 = 0
+	for i := 0; i < len(data.tx.Vout); i++ {
+		txOut := &data.tx.Vout[i]
+
+		if txOut.ScriptPubKey.Address == data.invoice.CryptoAddress ||
+			(len(txOut.ScriptPubKey.Addresses) == 1 && txOut.ScriptPubKey.Addresses[0] == data.invoice.CryptoAddress) {
+			amount += data.tx.Vout[i].Value
+		}
+	}
+
+	return amount, nil
+}
+
+func generateNextLTCAddressHandler(ctx context.Context, q *db.Queries, data *generateNextAddressHandlerData) (db.CryptoAddress, error) {
+	var addr db.CryptoAddress
+
+	net, err := func() (*chaincfg.Params, error) {
+		switch data.network {
+		case listener.MainnetLTC:
+			return &chaincfg.MainNetParams, nil
+		case listener.TestnetLTC:
+			return &chaincfg.TestNet4Params, nil
+		case listener.SignetLTC:
+			return &chaincfg.SigNetParams, nil
+		case listener.RegtestLTC:
+			return &chaincfg.RegressionNetParams, nil
+		default:
+			return nil, errors.New("invalid network type")
+		}
+	}()
+	if err != nil {
+		return addr, err
+	}
+
+	cd, err := q.FindCryptoDataByUserId(ctx, data.userId)
+	if err != nil {
+		return addr, err
+	}
+
+	indices, err := q.FindIndicesAndLockLTCCryptoDataById(ctx, cd.LtcID)
+	if err != nil {
+		return addr, err
+	}
+
+	mPubStr, err := q.FindKeysAndLockLTCCryptoDataById(ctx, cd.LtcID)
+	if err != nil {
+		return addr, err
+	}
+
+	mPub, err := hdkeychain.NewKeyFromString(mPubStr)
+	if err != nil {
+		return addr, err
+	}
+
+	indices.LastMinorIndex++
+	if indices.LastMinorIndex <= 0 {
+		indices.LastMinorIndex = 0
+		indices.LastMajorIndex++
+	}
+
+	majMPub, err := mPub.Derive(uint32(indices.LastMajorIndex))
+	if err != nil {
+		return addr, err
+	}
+	minMPub, err := majMPub.Derive(uint32(indices.LastMinorIndex))
+	if err != nil {
+		return addr, err
+	}
+
+	pubKey, err := minMPub.ECPubKey()
+	if err != nil {
+		return addr, err
+	}
+
+	pubKeyHash := ltcutil.Hash160(pubKey.SerializeCompressed())
+	newAddr, err := ltcutil.NewAddressWitnessPubKeyHash(pubKeyHash, net)
+	if err != nil {
+		return addr, err
+	}
+
+	addr, err = q.CreateCryptoAddress(ctx, db.CreateCryptoAddressParams{Address: newAddr.EncodeAddress(), Coin: db.CoinTypeLTC, IsOccupied: true, UserID: data.userId})
+	if err != nil {
+		return addr, err
+	}
+
+	if _, err := q.UpdateIndicesLTCCryptoDataById(ctx, db.UpdateIndicesLTCCryptoDataByIdParams{ID: cd.LtcID, LastMajorIndex: indices.LastMajorIndex, LastMinorIndex: indices.LastMinorIndex}); err != nil {
+		return addr, err
+	}
+
+	return addr, nil
+}
+
+func newLtcProcessor(log *zerolog.Logger, dbConnPool *pgxpool.Pool, invoiceCn chan<- db.Invoice, c *dto.DaemonsConfig) (*ltcProcessor, error) {
+	u, err := url.Parse(c.Ltc.Url)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &rpcclient.ConnConfig{
+		Host:         u.Host + u.RequestURI(),
+		User:         c.Ltc.User,
+		Pass:         c.Ltc.Pass,
+		DisableTLS:   u.Scheme != "https",
+		HTTPPostMode: true,
+	}
+	client, err := rpcclient.New(conf, nil)
+	if err != nil {
+		return nil, err
+	}
+	ltcClient, err := ltcrpc.New(&ltcrpc.ConnConfig{
+		Host:         conf.Host,
+		User:         conf.User,
+		Pass:         conf.Pass,
+		DisableTLS:   conf.DisableTLS,
+		HTTPPostMode: conf.HTTPPostMode,
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	base, err := newBaseCryptoProcessor(
+		log,
+		dbConnPool,
+		invoiceCn,
+		listener.NewSharedLTCDaemonRpcClient(client, ltcClient),
+		verifyLTCTxHandler,
+		generateNextLTCAddressHandler,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ltcProcessor{baseCryptoProcessor: *base}, nil
+}
---
+++ internal/processor/ltc_integration_test.go
@@ -0,0 +1,208 @@
+package processor
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"testing"
+	"time"
+
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/chekist32/goipay/internal/db"
+	"github.com/chekist32/goipay/internal/listener"
+	"github.com/chekist32/goipay/test"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	ltcrpc "github.com/ltcsuite/ltcd/rpcclient"
+	"github.com/stretchr/testify/assert"
+)
+
+func createUserWithLtcData(ctx context.Context, q *db.Queries) (pgtype.UUID, db.CryptoDatum, db.LtcCryptoDatum) {
+	userId, err := q.CreateUser(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ltcData, err := q.CreateLTCCryptoData(ctx, "zpub6o5L7tQbC4zavTL1Lzq1eg5qev4WQMXNWMGoruoHSX8YRss8V4U1k4UUae8abXpVxNh9eBHTLBGBjvuCSRtfVtAmf4LRBtsNxQX4gpj56Dc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cd, err := q.CreateCryptoData(ctx, db.CreateCryptoDataParams{LtcID: ltcData.ID, UserID: userId})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return userId, cd, ltcData
+}
+
+func createNewTestLtcDaemon1() *rpcclient.Client {
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "api.chainup.net/litecoin/mainnet/0b1abdf17ecc4b20b110ee73e17e7493",
+		User:         "user",
+		Pass:         "pass",
+		HTTPPostMode: true,
+		DisableTLS:   false,
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func createNewTestLtcDaemon2() *ltcrpc.Client {
+	connCfg := &ltcrpc.ConnConfig{
+		Host:         "api.chainup.net/litecoin/mainnet/0b1abdf17ecc4b20b110ee73e17e7493",
+		User:         "user",
+		Pass:         "pass",
+		HTTPPostMode: true,
+		DisableTLS:   false,
+	}
+	client, err := ltcrpc.New(connCfg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func TestGenerateNextLtcAddressHandler(t *testing.T) {
+	t.Parallel()
+
+	data := []struct {
+		prevMajorIndex int32
+		prevMinorIndex int32
+		expectedAddr   string
+	}{
+		{
+			prevMajorIndex: 0,
+			prevMinorIndex: 0,
+			expectedAddr:   "ltc1quc3y9flfnhd9zeg5dm5h4ckzz040ykk495rstl",
+		},
+		{
+			prevMajorIndex: 0,
+			prevMinorIndex: 124,
+			expectedAddr:   "ltc1qu0a864puuay8hsz9sg5sjl92d5qsxlp9w6740v",
+		},
+		{
+			prevMajorIndex: 0,
+			prevMinorIndex: math.MaxInt32,
+			expectedAddr:   "ltc1quhh8zju3ah6hcjq624nvudfd0sevn9g3u80y2r",
+		},
+		{
+			prevMajorIndex: 1,
+			prevMinorIndex: 2,
+			expectedAddr:   "ltc1qqnla6k30dly8vj370jqmx0ytynwrc92uxvxd45",
+		},
+	}
+
+	ctx := context.Background()
+
+	dbConn, _, close := getPostgresWithDbConn()
+	defer close(ctx)
+
+	for _, d := range data {
+		t.Run(fmt.Sprintf("Should Return Valid Address Ma %v Mi %v", d.prevMajorIndex, d.prevMinorIndex), func(t *testing.T) {
+			test.RunInTransaction(t, dbConn, func(t *testing.T, tx pgx.Tx) {
+				// Given
+				q := db.New(dbConn).WithTx(tx)
+				userId, cd, _ := createUserWithLtcData(ctx, q)
+
+				_, err := q.UpdateIndicesLTCCryptoDataById(ctx, db.UpdateIndicesLTCCryptoDataByIdParams{ID: cd.LtcID, LastMajorIndex: d.prevMajorIndex, LastMinorIndex: d.prevMinorIndex})
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// When
+				addr, err := generateNextLTCAddressHandler(ctx, q, &generateNextAddressHandlerData{userId: userId, network: listener.MainnetLTC})
+
+				// Assert
+				assert.NoError(t, err)
+				assert.Equal(t, d.expectedAddr, addr.Address)
+			})
+		})
+	}
+
+}
+
+func TestVerifyLTCTxHandler(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	dbConn, _, close := getPostgresWithDbConn()
+	defer close(ctx)
+
+	daemon := listener.NewSharedLTCDaemonRpcClient(createNewTestLtcDaemon1(), createNewTestLtcDaemon2())
+
+	t.Run("Should Return Right Amount (Valid Tx)", func(t *testing.T) {
+		test.RunInTransaction(t, dbConn, func(t *testing.T, tx pgx.Tx) {
+			// Given
+			q := db.New(dbConn).WithTx(tx)
+			userId, _, _ := createUserWithXmrData(ctx, q)
+
+			expectedTxId := "4c699b97e516791e9189211af52f0f18fb24d71e86fb73530dfc9fc1fb00fc33"
+			expectedInvoice, err := q.CreateInvoice(ctx, db.CreateInvoiceParams{
+				UserID:                userId,
+				Coin:                  db.CoinTypeLTC,
+				CryptoAddress:         "ltc1qa9fetyxs65t03w32vfyen4w2nph9uq9wr7pmg4",
+				RequiredAmount:        3.05398200,
+				ExpiresAt:             pgtype.Timestamptz{Time: time.Now().Add(time.Minute), Valid: true},
+				ConfirmationsRequired: 0,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			txs, err := daemon.GetTransactions([]string{expectedTxId})
+			if err != nil {
+				log.Fatal(err)
+			} else if len(txs) < 1 {
+				log.Fatalf("Invalid Tx Id: %v", expectedTxId)
+			}
+
+			// When
+			amount, err := verifyLTCTxHandler(ctx, q, &verifyTxHandlerData[listener.LTCTx]{invoice: expectedInvoice, tx: txs[0]})
+
+			// Assert
+			assert.NoError(t, err)
+			assert.Equal(t, expectedInvoice.RequiredAmount, amount)
+		})
+	})
+
+	t.Run("Should Return 0 Amount (Invalid Tx)", func(t *testing.T) {
+		test.RunInTransaction(t, dbConn, func(t *testing.T, tx pgx.Tx) {
+			// Given
+			q := db.New(dbConn).WithTx(tx)
+			userId, _, _ := createUserWithLtcData(ctx, q)
+
+			expectedTxId := "16132b08000b8af8e4ff5e63d1b10d2cf730ace428c778b4da44080b4ccc58a8"
+			expectedInvoice, err := q.CreateInvoice(ctx, db.CreateInvoiceParams{
+				UserID:                userId,
+				Coin:                  db.CoinTypeLTC,
+				CryptoAddress:         "ltc1qa9fetyxs65t03w32vfyen4w2nph9uq9wr7pmg4",
+				RequiredAmount:        3.05398200,
+				ExpiresAt:             pgtype.Timestamptz{Time: time.Now().Add(time.Minute), Valid: true},
+				ConfirmationsRequired: 0,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			txs, err := daemon.GetTransactions([]string{expectedTxId})
+			if err != nil {
+				log.Fatal(err)
+			} else if len(txs) < 1 {
+				log.Fatalf("Invalid Tx Id: %v", expectedTxId)
+			}
+
+			// When
+			amount, err := verifyLTCTxHandler(ctx, q, &verifyTxHandlerData[listener.LTCTx]{invoice: expectedInvoice, tx: txs[0]})
+
+			// Assert
+			assert.NoError(t, err)
+			assert.Equal(t, float64(0), amount)
+		})
+	})
+}
